1b8606349a3c9707ef161f469368988d6f14ef7f
Stepan Henek <stepan.henek@nic.cz>
2019-04-02 08:44:38 +0200

    rewritten to use pathlib + create sass directory when missing

--- foris_plugins_distutils/__init__.py
+++ foris_plugins_distutils/__init__.py
@@ -1,7 +1,7 @@
 import copy
-import glob
-import os
 import shutil
+import typing
+import pathlib
 
 from distutils import log
 from distutils.cmd import Command
@@ -11,7 +11,7 @@ class ForisPluginCommand(Command):
     def get_plugin_name(self):
         for package in self.distribution.packages:
             if package.startswith("foris_plugins."):
-                return package[len("foris_plugins."):]
+                return package[len("foris_plugins.") :]
         raise RuntimeError(
             "This package doesn't seem like a Foris plugin. No foris plugin package found!"
         )
@@ -21,12 +21,12 @@ class make_messages(ForisPluginCommand):
     """Extracts and merges transation of foris plugin"""
 
     MESSAGE_EXTRACTORS = [
-        ('**.py', 'python', None),
-        ('**/templates/**.j2', 'jinja2', {'encoding': 'utf-8'}),
+        ("**.py", "python", None),
+        ("**/templates/**.j2", "jinja2", {"encoding": "utf-8"}),
     ]
 
     description = __doc__
-    user_options = []
+    user_options: typing.List[str] = []
 
     def initialize_options(self):
         pass
@@ -38,13 +38,10 @@ class make_messages(ForisPluginCommand):
         from babel.messages import frontend
 
         plugin_name = self.get_plugin_name()
-        plugin_path = os.path.join("foris_plugins", plugin_name)
+        plugin_path = pathlib.Path("foris_plugins") / plugin_name
 
         # make sure that locale directory exists
-        try:
-            os.makedirs(os.path.join(plugin_path, "locale"))
-        except os.error:
-            pass  # already exists
+        (plugin_path / "locale").mkdir(parents=True, exist_ok=True)
 
         # set message extractors in distribution
         distribution = copy.copy(self.distribution)
@@ -54,7 +51,7 @@ class make_messages(ForisPluginCommand):
         log.info("Starting to extract messages for foris plugin")
         cmd = frontend.extract_messages(copy.copy(distribution))
         cmd.no_location = True
-        cmd.output_file = os.path.join(plugin_path, "locale", "foris.pot")
+        cmd.output_file = plugin_path / "locale/foris.pot"
         cmd.ensure_finalized()
         cmd.run()
 
@@ -62,8 +59,8 @@ class make_messages(ForisPluginCommand):
         log.info("Starting to update foris translations")
         cmd = frontend.update_catalog(copy.copy(distribution))
         cmd.domain = "foris"
-        cmd.input_file = os.path.join(plugin_path, "locale", "foris.pot")
-        cmd.output_dir = os.path.join(plugin_path, "locale")
+        cmd.input_file = plugin_path / "locale/foris.pot"
+        cmd.output_dir = plugin_path / "locale"
         cmd.ensure_finalized()
         cmd.run()
 
@@ -72,7 +69,7 @@ class clean(ForisPluginCommand):
     """Removes files created by foris commands"""
 
     description = __doc__
-    user_options = []
+    user_options: typing.List[str] = []
 
     def initialize_options(self):
         pass
@@ -82,33 +79,31 @@ class clean(ForisPluginCommand):
 
     def run(self):
         plugin_name = self.get_plugin_name()
-        plugin_path = os.path.join("foris_plugins", plugin_name)
+        plugin_path = pathlib.Path("foris_plugins") / plugin_name
 
         # remove all css files (excpecting that all .css file are generated using sass)
-        css_path = os.path.join(plugin_path, "static", "css", "*.css")
-        for path in glob.glob(css_path):
+        css_path = plugin_path / "static/css"
+        for path in css_path.glob("*.css"):
             log.info("Removing css file %s." % path)
             try:
-                os.remove(path)
+                path.unlink()
             except OSError:
                 pass
 
         # remove generated translations
-        generated_translations_path = os.path.join(
-            plugin_path, "locale", "*", "LC_MESSAGES", "messages.po")
-        for path in glob.glob(generated_translations_path):
+        locale_path = plugin_path / "locale"
+        for path in locale_path.glob("*/LC_MESSAGES/messages.po"):
             log.info("Removing generated translation file %s." % path)
             try:
-                os.remove(path)
+                path.unlink()
             except OSError:
                 pass
 
         # remove compiled translations
-        compiled_translations_path = os.path.join(plugin_path, "locale", "*", "LC_MESSAGES", "*.mo")
-        for path in glob.glob(compiled_translations_path):
+        for path in locale_path.glob("*/LC_MESSAGES/*.mo"):
             log.info("Removing compiled translation file %s." % path)
             try:
-                os.remove(path)
+                path.unlink()
             except OSError:
                 pass
 
@@ -117,7 +112,7 @@ class build(ForisPluginCommand):
     """Builds all foris files needed before creating a packages"""
 
     description = __doc__
-    user_options = []
+    user_options: typing.List[str] = []
 
     def initialize_options(self):
         pass
@@ -127,22 +122,21 @@ class build(ForisPluginCommand):
 
     def run(self):
         plugin_name = self.get_plugin_name()
-        plugin_path = os.path.join("foris_plugins", plugin_name)
+        plugin_path = pathlib.Path("foris_plugins") / plugin_name
 
         # rename foris.po to messages.po to match the foris domain
         catalog_exists = False
-        for path in glob.glob(
-            os.path.join(plugin_path, "locale", "*", "LC_MESSAGES", "foris.po")
-        ):
+        for path in plugin_path.glob("locale/*/LC_MESSAGES/foris.po"):
             catalog_exists = True
-            shutil.copyfile(path, os.path.join(os.path.dirname(path), "messages.po"))
+            shutil.copyfile(path, pathlib.Path(path).parent / "messages.po")
 
         if catalog_exists:
             # compile translation
             from babel.messages import frontend as babel
+
             distribution = copy.copy(self.distribution)
             cmd = babel.compile_catalog(distribution)
-            cmd.directory = os.path.join(plugin_path, "locale")
+            cmd.directory = plugin_path / "locale"
             cmd.domain = "messages"
             cmd.ensure_finalized()
             cmd.run()
@@ -151,19 +145,23 @@ class build(ForisPluginCommand):
             log.info("To create translations you need to generate .pot file:")
             log.info("    python setup.py foris_make_messages")
             log.info("And prepare at least one catalog:")
-            out_dir = os.path.join(plugin_path, "locale")
-            pot_file = os.path.join(out_dir, "foris.pot")
+            out_dir = plugin_path / "locale"
+            pot_file = out_dir / "foris.pot"
             log.info(
-                "    python setup.py init_catalog -D foris -i %s -d %s -l <lang>",
-                pot_file, out_dir
+                "    python setup.py init_catalog -D foris -i %s -d %s -l <lang>", pot_file, out_dir
             )
 
         # compile sass
         from sassutils import distutils as sass
+
+        # make sure that sass directory exists
+        (plugin_path / "static/sass").mkdir(parents=True, exist_ok=True)
+
         distribution = copy.copy(self.distribution)
         setattr(
-            distribution, "sass_manifests",
-            {"foris_plugins." + plugin_name: ('static/sass/', 'static/css/')}
+            distribution,
+            "sass_manifests",
+            {"foris_plugins." + plugin_name: ("static/sass/", "static/css/")},
         )
         cmd = sass.build_sass(distribution)
         # cmd.output_style = "compressed"
@@ -171,6 +169,6 @@ class build(ForisPluginCommand):
         cmd.run()
         # rename xxx.sass.css -> xxx.css
         # there is a MR on github to strip it, but in hasn't been merge yet
-        for path in glob.glob(os.path.join(plugin_path, "static", "css", "*.sass.css")):
-            filename = os.path.basename(path)[:-len("sass.css")] + "css"
-            os.rename(path, os.path.join(os.path.dirname(path), filename))
+        for path in plugin_path.glob("static/css/*.sass.css"):
+            new_filename = path.name[: -len("sass.css")] + "css"
+            path.rename(path.parent / new_filename)
--- setup.py
+++ setup.py
@@ -4,29 +4,21 @@ from setuptools import setup
 
 setup(
     name="Foris Plugins Distutils",
-    version="0.1",
+    version="0.3",
     description="Distutils patches to add commands for foris plugin handling",
     author="CZ.NIC, z. s. p. o.",
     author_email="stepan.henek@nic.cz",
     url="https://gitlab.labs.nic.cz/turris/foris-plugins-distutils/",
     license="GPL-3.0",
     requires=[],
-    install_requires=[
-        'babel',
-        'jinja2',
-        'libsass',
-    ],
-    provides=[
-        "foris_plugins_distutils",
-    ],
-    packages=[
-        "foris_plugins_distutils",
-    ],
+    install_requires=["babel", "jinja2", "libsass"],
+    provides=["foris_plugins_distutils"],
+    packages=["foris_plugins_distutils"],
     entry_points={
         "distutils.commands": [
             "foris_make_messages = foris_plugins_distutils:make_messages",
             "foris_clean = foris_plugins_distutils:clean",
             "foris_build = foris_plugins_distutils:build",
-        ],
+        ]
     },
 )
